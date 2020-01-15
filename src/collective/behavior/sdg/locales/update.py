9bc97c7c8f6caeeaf0c79f29a721edff481f950b
Stephan Geulette <s.geulette@imio.be>
2020-01-15 13:26:28 +0100

    Added registry content

--- src/collective/behavior/sdg/locales/update.py
+++ src/collective/behavior/sdg/locales/update.py
@@ -8,7 +8,7 @@ import subprocess
 domain = 'collective.behavior.sdg'
 os.chdir(pkg_resources.resource_filename(domain, ''))
 os.chdir('../../../')
-target_path = 'src/collective/sdg/'
+target_path = 'src/collective/behavior/sdg/'
 locale_path = target_path + 'locales/'
 i18ndude = './bin/i18ndude'
 
